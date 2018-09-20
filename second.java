b84e241f232b8be9145842c391963ab51eb58bf2
garikapatijaswitha <41731218+garikapatijaswitha@users.noreply.github.com>
2018-09-20 10:56:13 +0530

    Create second.java

---
+++ second.java
@@ -0,0 +1,17 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		String hx=sc.next();
+		int n=Integer.parseInt(hx,2);
+		System.out.println(n);
+	}
+}
