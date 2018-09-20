94493bec9442289ac5ed9765121472d1e0cf5d20
garikapatijaswitha <41731218+garikapatijaswitha@users.noreply.github.com>
2018-09-20 10:54:19 +0530

    Create 92.java

---
+++ 92.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		String hx=sc.next();
+		int n=Integer.parseInt(hx,2);
+		System.out.println(n);
+	}
+}
